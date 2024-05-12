8e46f2bed85e1d117cb83a8626364825d29271de
geraltigas <jb030112@outlook.com>
2024-05-12 20:37:34 +0800

    update: begin to develop new schedule algorithm

---
+++ src/component/scheduler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jb030 on 12/05/2024.
+//
+
+#include "scheduler.h"
---
+++ src/component/scheduler.h
@@ -0,0 +1,14 @@
+//
+// Created by jb030 on 12/05/2024.
+//
+
+#ifndef ATODO_SERVICE_SCHEDULER_H
+#define ATODO_SERVICE_SCHEDULER_H
+
+
+namespace scheduler {
+
+};
+
+
+#endif //ATODO_SERVICE_SCHEDULER_H
